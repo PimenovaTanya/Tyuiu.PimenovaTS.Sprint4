95d3cb8c0569c137a7a5b0799183d1fbf478eb14
tyuiu.pimenovaTS@outlook.com <Tatyana.filia.2005.pimenova@mail.ru>
2023-10-31 17:27:19 +0500

    Task2

---
+++ Tyuiu.PimenovaTS.Sprint4.Task2.V9.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.PimenovaTS.Sprint4.Task2.V9.Lib;
+
+namespace Tyuiu.PimenovaTS.Sprint4.Task2.V9.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            int[] lol = { 4, 9, 2, 3, 7, 1 };
+
+            int res = ds.Calculate(lol);
+            int wait = 8;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
